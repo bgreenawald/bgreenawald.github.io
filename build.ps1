0d4e990e5d6344f38a25ce63d85948c73b1d18b7
Benjamin Greenawald <bgreenawald@gmail.com>
2017-12-22 22:57:49 -0500

    Added build script

---
+++ build.ps1
@@ -0,0 +1,24 @@
+﻿$message=$args[0]
+
+# Remove current pdf
+Remove-Item Resume_2017.pdf
+
+# Change to job search directory
+cd "C:\Users\Ben\Documents\Job-Search"
+
+# Pull the current version of the resume
+git pull
+
+# Copy it over
+Copy-Item Resume_2017.pdf C:\xampp\htdocs\bgreenawald.github.io\Resume_2017.pdf
+
+# Change back
+cd "C:\xampp\htdocs\bgreenawald.github.io\"
+
+# Run the blog build script
+python generate.py
+
+# Add and push
+git add . 
+git commit -m $message
+git push
