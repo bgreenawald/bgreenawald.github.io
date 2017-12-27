4414e4eb503c1d7d2fe593c57f1d4a7321c90451
Benjamin Greenawald <bgreenawald@gmail.com>
2017-12-27 11:37:32 -0500

    Blog posts switched to HTML

--- generate.py
+++ generate.py
@@ -13,22 +13,22 @@ for subdir, dirs, files in os.walk("blog"):
             # For each file, get the necesary info and append to our dict
             text = file_name.read()
 
-            name = re.findall("name = .+;", text)
-            name = name[0].replace("name = \"", "").replace("\";", "")
+            name = re.findall("titleBegin [\"\<\>a-zA-Z ,.\n\/?\-()0-9!']+ titleEnd", text)
+            name = name[0].replace("titleBegin -->", "").replace("<!-- titleEnd", "")
 
-            content = re.findall("content = [\"\<\>a-zA-Z ,.\n\/?\-()0-9!']+;", text)
-            content = content[0].replace("content = \"", "").replace("\";", "")
+            content = re.findall("contentBegin [\"\<\>a-zA-Z ,.\n\/?\-()0-9!\t']+ contentEnd", text)
+            content = content[0].replace("contentBegin -->", "").replace("<!-- contentEnd", "")
 
-            date = re.findall("date = .+;", text)
-            date = date[0].replace("date = \"", "").replace("\";", "")
+            date = re.findall("dateBegin [\"\<\>a-zA-Z ,.\n\/?\-()0-9!']+ dateEnd", text)
+            date = date[0].replace("dateBegin ", "").replace(" dateEnd", "")
 
-            path = re.findall("path = .+;", text)
-            path = path[0].replace("path = \"", "").replace("\";", "")
+            path = re.findall("pathBegin [\"\<\>a-zA-Z ,.\n\/?\-()0-9!']+ pathEnd", text)
+            path = path[0].replace("pathBegin ", "").replace(" pathEnd", "")
 
             dicts.append({"name": name, "content": content, "date": date, "location": path})
             file_name.close()
 
 # Write the file to the json object
 with open("data.json", "w+") as file:
-    file.write(json.dumps(dicts))
+    file.write(json.dumps(dicts, sort_keys=True, indent=4, separators=(',', ': ')))
     file.close()
